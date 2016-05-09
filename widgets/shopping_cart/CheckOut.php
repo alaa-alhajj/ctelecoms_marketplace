e5f5858df808240096893bfc7ba8180f931cd13e
haider91 <haider.ali.issa@gmail.com>
2016-05-09 10:07:37 +0200

    H Comm
    add ctelecom-style_haider.css
    and add sign up section in check out page

--- widgets/shopping_cart/CheckOut.php
+++ widgets/shopping_cart/CheckOut.php
@@ -15,6 +15,43 @@ if ($_POST['LoginC'] === 'login' && $_REQUEST['username'] != "" && $_REQUEST['pa
           $utils->redirect(_PREF.$pLang."/page58/Payment");
     }
 }
+
+if(isset($_REQUEST['SignUp'],$_REQUEST['full_name'],$_REQUEST['email'],$_REQUEST['password'],$_REQUEST['repeat_password']) && $_REQUEST['SignUp']=='signup'){
+            
+            $_REQUEST['submit']='submited';       
+            $full_name=  addslashes($_REQUEST['full_name']);
+            $email=addslashes($_REQUEST['email']);
+            $company=addslashes($_REQUEST['company']);
+            $city=addslashes($_REQUEST['city']);
+            $adress=addslashes($_REQUEST['adress']);
+            $password=$_REQUEST['password'];
+            $repeat_password=$_REQUEST['repeat_password'];
+            $successMSG='';
+            $ErrorMSG='';
+            if($password===$repeat_password){
+                global $utils;
+                global $pLang;
+                
+                $insert_id = $this->fpdo->insertInto('customers')->values(array('name'=>$full_name,'email'=>$email,'password'=>md5($password),'company'=>$company,'city'=>$city,'adress'=>$adress))->execute(); 
+                if($insert_id!=''){
+                    
+                    $successMSG="<div class='alert alert-success'>Successful, welcome <strong>$full_name<strong> in our site  </div>"; 
+                    @session_start();
+                    $_SESSION['CUSTOMER_Name'] = $full_name;
+                    $_SESSION['CUSTOMER_ID'] = $insert_id;
+                    $utils->redirect(_PREF.$pLang."/page49/My-Account");
+                    
+                }else{
+                    $ErrorMSG="<div class='alert alert-danger'>Error, this Email is aleady exist.</div>"; 
+                }
+                
+                
+            }else{
+               $ErrorMSG="<div class='alert alert-danger'>Error, password don't match repeat password. Enter password again.</div>"; 
+            }
+            
+        }
+        
 if($_SESSION['CUSTOMER_ID'] !=""){
      $this->redirect(_PREF.$_SESSION['pLang']."/page58/Payment");
 
@@ -35,6 +72,21 @@ if($_SESSION['CUSTOMER_ID'] !=""){
         <div class="row row-nomargin">
             <div class="col-sm-6 nopadding">
                 <h1>New Customer</h1>
+                <div class="login-block sign-up-block">
+                        <h1>Sign up</h1>
+                        <form class="form-horizontal" role="form" method="post" action="">
+                            <?=$successMSG?>
+                            <?=$ErrorMSG?>
+                            <input type="text"  name='full_name' id="full_name" placeholder="Full name" required>
+                            <input type="text"  name='company' id="company" placeholder="Company">
+                            <input type="text"  name='city' id="city" placeholder="City">
+                            <input type="text"  name='adress' id="adress" placeholder="Adress">
+                            <input type="email"  name='email' id="email" placeholder="Email" required>
+                            <input type="password"  name='password' id="password" placeholder="Password" required>
+                            <input type="password"  name='repeat_password' id="repeat_password" placeholder="Password again" required>
+                            <button type="submit"  id="submit" name='SignUp' value="signup">Sign Up</button>
+                        </form>
+                </div>
             </div>
             <div class="col-sm-6 nopadding">
                 <h1>Old Customer</h1>
